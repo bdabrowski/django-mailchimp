61346c5c0e96bfeda485b1fdd6ef16e5a9f69c3c
Bartosz <bar.dabrowski@gmail.com>
2015-03-13 11:56:20 +0100

    Updated setup.py and version.

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 version = __import__('mailchimp').__version__
 
 setup(
-    name = 'django-mailchimp-v1.3',
-    version = version,
-    description = 'Mailchimp wrapper for Django, using Mailchimp API 1.3',
-    author = 'Jonas Obrist et al.',
-    url = 'http://github.com/piquadrat/django-mailchimp',
-    packages = find_packages(),
+    name='django-mailchimp-v1.3',
+    version=version,
+    description='Mailchimp wrapper for Django, using Mailchimp API 1.3',
+    author='Bartosz Dabrowski',
+    url='http://github.com/bdabrowski/django-mailchimp',
+    packages=find_packages(),
     zip_safe=False,
     package_data={
         'mailchimp': [
